dd1a4f1138c12a20c92bb727e4d8902215a83f6d
Ugo DANIEL-THOMAS <ugdaniel@e2r8p4.clusters.42paris.fr>
2022-02-25 12:02:28 +0100

    fix segfault when no args

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,11 +6,12 @@
 /*   By: ugdaniel <ugdaniel@42.student.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 13:12:54 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/02/25 11:55:55 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/02/25 12:01:33 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <string.h>
 
 void	clear_mutexes(t_rules *r, int32_t max)
 {
@@ -74,6 +75,7 @@ int	main(int ac, const char **av)
 	int		done;
 	t_rules	rules;
 
+	memset(&rules, 0, sizeof(t_rules));
 	if (ac < 5 || ac > 6)
 		exit_error(&rules, 1);
 	done = init_rules(&rules, ac, av);
