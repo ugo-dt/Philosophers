c733e0a93e1e4cd9088043f7ef72592507c26b85
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-03-03 12:37:12 +0100

    fixed bug where all philos take one fork

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 18:01:43 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/03/03 10:32:36 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/03/03 12:36:10 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ static void	philo_loop(t_philo *p)
 	int32_t		count;
 	int32_t		max;
 
+	if (p->nb % 2)
+		usleep(500);
 	count = 0;
 	max = p->rules->nb_must_eat;
 	while (1)
@@ -41,8 +43,6 @@ void	*phi(void *philo)
 	t_philo		*p;
 
 	p = (t_philo *)philo;
-	if (p->nb % 2)
-		usleep(100);
 	p->last_meal = get_time();
 	if ((pthread_create(&p->check_death, NULL, &check_death, p)) != 0)
 	{
@@ -73,13 +73,14 @@ int	start(t_rules *r)
 	i = 0;
 	while (i < r->nb_philo)
 		pthread_join(r->philo[i++].thread_id, NULL);
+	pthread_mutex_lock(&r->mutex_dead);
 	if (r->nb_must_eat && !r->dead)
 	{
-		pthread_mutex_lock(&r->mutex_dead);
 		r->dead = 1;
 		printf("%llu\tAll philosophers have eaten ",
 			get_time() - r->start_time);
 		done_eating(r->nb_must_eat);
 	}
+	pthread_mutex_unlock(&r->mutex_dead);
 	return (EXIT_SUCCESS);
 }
