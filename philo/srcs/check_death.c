81e81570023721fc1eaa078233732dd0bf6841c2
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-03-03 21:11:09 +0100

    fixed 2 'died'

--- philo/srcs/check_death.c
+++ philo/srcs/check_death.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_death.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ugdaniel <ugdaniel@42.student.fr>          +#+  +:+       +#+        */
+/*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 23:55:55 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/03/03 18:22:06 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/03/03 21:10:13 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ static int	check_time(t_philo *p)
 	if (get_time() > p->last_meal + (t_ull)p->rules->time_to_die)
 	{
 		pthread_mutex_lock(&p->rules->mutex_dead);
-		p->rules->dead = 1;
 		pthread_mutex_lock(&p->rules->logs);
+		if (!p->rules->dead)
 		printf("%llu\t%u died\n",
 			get_time() - p->rules->start_time, p->nb + 1);
+		p->rules->dead = 1;
 		pthread_mutex_unlock(&p->eating);
 		pthread_mutex_unlock(&p->rules->mutex_dead);
 		pthread_mutex_unlock(&p->rules->logs);
@@ -47,7 +48,7 @@ void	*check_death(void *philo)
 		pthread_mutex_unlock(&p->rules->mutex_dead);
 		if (check_time(p))
 			return (NULL);
-		usleep(100);
+		ft_sleep(p, 1);
 	}
 	return (NULL);
 }
